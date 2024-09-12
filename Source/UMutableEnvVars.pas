aa35ddf968f2b936e44c0c32cc158b71ea1b3882
delphidabbler <5164283+delphidabbler@users.noreply.github.com>
2024-09-12 13:49:10 +0100

    Add MPL license blocks to new source files
    Three new source code files had not had MPL 2.0 license comment blocks
    added when the file were created.

--- Source/UMutableEnvVars.pas
+++ Source/UMutableEnvVars.pas
@@ -1,3 +1,15 @@
+{
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/
+ *
+ * Copyright (C) 2024, Peter Johnson (www.delphidabbler.com).
+ *
+ * Implments a class that records system environment variables and permits their
+ * values to be updated.
+}
+
+
 unit UMutableEnvVars;
 
 interface
