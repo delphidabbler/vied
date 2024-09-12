ef1446e5414ab7dfe94a00c8896547b0a57fa633
delphidabbler <5164283+delphidabbler@users.noreply.github.com>
2024-09-12 13:50:24 +0100

    Update copyright date in MPL 2.0 comment blocks
    Updated copyright date in source files modified during 2024 that had
    the copyrught date updated.

--- Source/FmMacroEd.pas
+++ Source/FmMacroEd.pas
@@ -3,7 +3,7 @@
  * v. 2.0. If a copy of the MPL was not distributed with this file, You can
  * obtain one at http://mozilla.org/MPL/2.0/
  *
- * Copyright (C) 2022-2023, Peter Johnson (https://delphidabbler.com).
+ * Copyright (C) 2022-2024, Peter Johnson (https://delphidabbler.com).
  *
  * Macro editor dialogue box.
 }
